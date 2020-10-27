168eb2489f47dba6ad3dbdb187afdad54bd0cad6
Stefanos <stefanosu422@gmail.com>
2020-10-27 13:37:24 -0400

    Updates

---
+++ precache-manifest.ca0170fcfcd2da38f217c0f9ab30b96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7b0d1ec2c2d091671fd61c9296365d",
+    "url": "/stefanosu/redux-frontendcatalog/index.html"
+  },
+  {
+    "revision": "ec838fc23f6e70cd2e29",
+    "url": "/stefanosu/redux-frontendcatalog/static/css/main.1dfeb340.chunk.css"
+  },
+  {
+    "revision": "eadc20b72ad39a98f279",
+    "url": "/stefanosu/redux-frontendcatalog/static/js/2.7420ae9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stefanosu/redux-frontendcatalog/static/js/2.7420ae9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec838fc23f6e70cd2e29",
+    "url": "/stefanosu/redux-frontendcatalog/static/js/main.ddcb8420.chunk.js"
+  },
+  {
+    "revision": "8e79a6412b2fc9d6b581",
+    "url": "/stefanosu/redux-frontendcatalog/static/js/runtime-main.83197b26.js"
+  }
+]);
